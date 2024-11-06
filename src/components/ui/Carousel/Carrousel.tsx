267bbf37f713643cc08e2522f2ebd197c591dd6b
vicente almeida <vicente.cra45@gmail.com>
2024-11-06 17:41:30 +0400

    Animation for selected element

--- src/components/ui/Carousel/Carrousel.tsx
+++ src/components/ui/Carousel/Carrousel.tsx
@@ -11,6 +11,9 @@ import './styles.css';
 // import required modules
 import { Autoplay } from 'swiper/modules';
 import { getImageURL } from '@/lib/utils';
+import clsx from 'clsx';
+
+import playIcon from '@/assets/playIcon.svg';
 
 interface Props {
     elements: {
@@ -23,7 +26,13 @@ interface Props {
   
 function Carousel( props : Props) {
     const { elements } = props;
+    const [hoveredId, setHoveredId] = useState<number | null>(null);
 
+    const handleClick = (id: number) => {
+        if(hoveredId === id){
+            console.log('clicked', id);
+        }
+    }
     return (
         <Swiper
                 slidesPerView={6}
@@ -44,9 +53,23 @@ function Carousel( props : Props) {
                 className="flex w-full h-[400px]"
             >
                 {elements.map((elem, index) => (
-                    <SwiperSlide key={index} className="flex w-full text-center text-white bg-black" style={{ transition: 'height 0.5s ease-in-out' }} onClick={() => {}}>
-                        <div className='flex flex-col items-center justify-center w-full h-full'>
+                    <SwiperSlide 
+                        key={index} 
+                        className={clsx("flex w-full text-center text-white bg-black")} 
+                        style={{ transition: 'height 0.5s ease-in-out' }}
+                        onMouseEnter={() => setHoveredId(index)}
+                        onClick={() => handleClick(index)}
+                    >
+                        <div className={clsx('flex flex-col items-center justify-center w-full h-full transition-transform duration-300', {
+                            'transform scale-105': hoveredId === index,
+                            'transform scale-100': hoveredId !== index,
+                        })}>
                             <img src={getImageURL(elem.poster_path)} alt={elem.title ? elem.title : elem.name} className='h-full w-full object-contain' />
+                            {hoveredId === index && (
+                                <div className="absolute inset-0 flex items-center justify-center">
+                                    <img src={playIcon} className="w-12 h-12" alt="Play icon" />
+                                </div>
+                            )}
                         </div>
                     </SwiperSlide>
                 ))}
